e1dc90bcfa078b26c83a6c2a4d373c9eb73de5a5
jonahsaltzman <jonahsaltzman@gmail.com>
2022-01-14 15:54:15 -0500

    cors policy working

--- src/IoServer.js
+++ src/IoServer.js
@@ -1,33 +1,23 @@
-const { Server } = require('socket.io')
 const Emitter = require('events')
 const UserSocket = require('./UserSocket')
 
 class Io {
-  constructor (server, name) {
-    this.name = name
+  constructor (server) {
     this.events = new Emitter()
-    this.server = new Server(server)
+    this.server = server
     this.server.on('connection', (socket) => {
       console.log('user connected')
-      this.socket = socket
-      this.socket.name = name
-      this.id = socket.id
       const newSocket = new UserSocket(socket)
       this.events.emit('new-connection', newSocket)
     })
   }
-  static create (server, name) {
-    const newServer = new this(server, name)
+  static create (server) {
+    const newServer = new this(server)
     console.log('created room')
     return newServer
   }
-  async addMember () {
-    console.log('request to join: ', this.name)
-    const joined = await this.socket.join(this.id)
-    console.log(joined)
-  }
-  // #server
-  // #socket
+  #server
+  #socket
 }
 
 module.exports = Io
