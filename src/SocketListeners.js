41afdd96c9707998b022a60bf405bdcbce3371cf
anthonyguariglia <aguariglia197@gmail.com>
2022-01-19 17:33:11 -0500

    socket listeners

--- src/SocketListeners.js
+++ src/SocketListeners.js
@@ -1,20 +1,44 @@
-const IoServer = require('./IoServer')
-// const UserSocket = require('./UserSocket')
 
-console.log(IoServer)
+// const UserSocket = require('./UserSocket')
 
 const connected = []
 
-IoServer.events.on('new-connection', (socket) => {
-  console.log('user joined')
-  console.log('socket connected: ')
-  connected.push(socket) // TODO: going to be a DB object
-})
+const newConnection = (socket) => {
+	console.log('socket connected: ')
+	connected.push(socket) // TODO: going to be a DB object
+}
 
-IoServer.events.on('join', async (req, socket) => {
-  await socket.join(req.roomId.toString())
+const destroySocket = (socketId) => {
+  const index = connected.findIndex(socket => socket.id === socketId)
+  if (index !== -1) {
+    connected.splice(index, 1)
+  }
+}
 
-  // for testing
-  IoServer.send('test', req.roomId.toString())
-  IoServer.send('test', 'jonahs-room')
-})
+const addListeners = (server) => {
+  console.log('calling addlisteners')
+  server.events.on('new-connection', newConnection)
+}
+
+const joinRoom = (userId, roomId) => {
+  // check if user is allowed to join room
+  // return true or false
+  console.log(`user ${userId} requesting to join room ${roomId}`)
+}
+
+const checkRoomAccess = (userID, roomId) => {
+  Room.findOne({ _id: roomId }, (err, room) => {
+    if (err) return false
+    if (room.validUsers.includes(userID)) {
+      return true
+    } else {
+      return false
+    }
+  })
+}
+
+module.exports = {
+  addListeners,
+  joinRoom,
+  destroySocket
+}
