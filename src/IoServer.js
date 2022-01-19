f37ef347a9ad39f31cb83ee67932b21af9805f2e
anthonyguariglia <aguariglia197@gmail.com>
2022-01-19 17:32:03 -0500

    ioserver

--- src/IoServer.js
+++ src/IoServer.js
@@ -2,22 +2,44 @@ const Emitter = require('events')
 const UserSocket = require('./UserSocket')
 
 class Io {
-  constructor (server) {
-    this.events = new Emitter()
-    this.server = server
-    this.server.on('connection', (socket) => {
-      console.log('user connected')
-      const newSocket = new UserSocket(socket)
-      this.events.emit('new-connection', newSocket)
-    })
-  }
-  static create (server) {
-    const newServer = new this(server)
-    console.log('created room')
-    return newServer
-  }
-  #server
-  #socket
+	constructor(server) {
+		this.events = new Emitter()
+		this.server = server
+		this.rooms = []
+		this.server.on('connection', (socket) => {
+			const newSocket = new UserSocket(socket, this)
+			this.events.emit('new-connection', newSocket)
+			// socket.on('connected', () => console.log('socket connected'))
+			// socket.on('connection', () => console.log('socket connected'))
+		})
+	}
+	static create(server) {
+		const newServer = new this(server)
+		// create one room for all users to use
+		newServer.rooms.push('all-users')
+		return newServer
+	}
+	findRoomByName(name) {
+		const room = this.rooms.find((room) => room.name === name)
+		return room
+	}
+	findRoomById(roomId) {
+		const room = this.rooms.find((room) => room.id === roomId)
+		return room
+	}
+	newRoom(roomName, roomId) {
+		const room = { name: roomName, id: roomId }
+		this.rooms.push(room)
+		return room
+	}
+	logEmitter() {
+		console.log(this.events)
+	}
+	sendMessage(msg) {
+		console.log('server sending message:')
+		console.log(msg)
+		this.server.to(msg.roomId).emit('message', msg)
+	}
 }
 
 module.exports = Io
