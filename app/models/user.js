681d22141374bd8a1b3ead92a59e419582286080
aymammeri <ay.mammeri@gmail.com>
2022-01-20 00:57:34 -0500

    Add status field on user model

--- app/models/user.js
+++ app/models/user.js
@@ -26,6 +26,10 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: false
 		},
+    status: {
+      type: String,
+      required: false
+    },
 		rooms: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
