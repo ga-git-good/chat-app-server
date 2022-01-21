45e694247a9d73c0ee0a557162390b0bb7ee99af
aymammeri <ay.mammeri@gmail.com>
2022-01-21 11:02:21 -0500

    Send status code 403 forbidden when user id doesn't match req body id

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -185,9 +185,11 @@ router.delete('/go-offline', requireToken, (req, res, next) => {
 router.delete('/delete-Account', requireToken, (req, res, next) => {
   if(req.body.id === req.user.id) {
     User.deleteOne({ _id: req.user.id })
-    .then(del => res.status(204).json({ del })) 
+    .then(del => res.status(204).json({ del }))
     .catch(next)
+  } else {
+    res.sendStatus(403)
   }
-}
+})
 
 module.exports = router
